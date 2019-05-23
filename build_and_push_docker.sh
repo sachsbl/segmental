c743a08ddc805b4f93c91a4bc238c282214e93d2
sachsbl <sachsbl@gmail.com>
2019-05-23 13:04:04 -0600

    RGBA support

--- build_and_push_docker.sh
+++ build_and_push_docker.sh
@@ -7,9 +7,6 @@ docker build -t segmental .
 docker tag paintly sachsbl/segmental:latest
 docker push sachsbl/segmental:latest
 
-$(aws ecr get-login --no-include-email --region us-east-1)
-docker tag segmental:latest 116226363472.dkr.ecr.us-east-1.amazonaws.com/segmental:latest
-docker push 116226363472.dkr.ecr.us-east-1.amazonaws.com/segmental:latest
 
 
 
